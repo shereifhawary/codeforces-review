b8d4a4be0c31e2d59395591e1c13673bd102a513
shereifhawary <shereifhawary@gmail.com>
2016-07-24 09:59:01 +0200

    adding project

---
+++ src/main/java/com/orange/internship/main/Main.java
@@ -0,0 +1,10 @@
+package com.orange.internship.main;
+
+/**
+ * Created by tdph5945 on 2016-07-24.
+ */
+public class Main {
+    public static void main(String... args) {
+        System.out.println("Hello, and Welcome :)");
+    }
+}
