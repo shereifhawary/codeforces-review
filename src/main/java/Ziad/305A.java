c02421e112636ab727136926fbecaa3dd3f4f3f5
ziad.mohamed <ziad_mohamed9473@hotmail.com>
2016-07-30 14:07:24 +0200

    Ziad's codes

---
+++ src/main/java/Ziad/305A.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int n=sc.nextInt(), sz=0, num = -1;
+		
+		int[] arr = new int[4];
+		
+		for(int i=0;i<4;++i)arr[i] = -1;
+		
+		for(int i=0;i<n;++i){
+			int x=sc.nextInt();
+			
+			if(x==0){
+				if(arr[0]==-1)arr[0]=1;
+				else ++arr[0];
+			}
+			if(x>0 && x<10)arr[1] = x;
+			if(x>=10 && x<100){
+				if(x%10==0)
+					arr[2] = x;
+				
+				else
+					num = x;
+			}
+			if(x==100)arr[3] = x;
+		}
+		
+		for(int i=1;i<4;++i)
+			if(arr[i]!=-1)
+				++sz;
+		
+		if(num!=-1 && (sz==0 || (sz==1 && arr[3]!=-1))){
+			arr[2] = num;
+			++sz;
+		}
+		
+		if(arr[0]>0)sz+=arr[0];
+		System.out.println(sz);
+		
+		while(arr[0]>0){
+			--arr[0];
+			System.out.print("0 ");
+		}
+		
+		for(int i=1;i<4;++i){
+			if(arr[i]!=-1)			
+				System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+		
+		sc.close();
+	}
+	
+	public static boolean check(int x){
+		if(x==0)return true;
+		
+		while(x>0){
+			if(x%10==0)
+				return true;
+			
+			x/=10;
+		}
+		return false;
+	}
+}
---
+++ src/main/java/Ziad/35A.java
@@ -0,0 +1,52 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			Scanner sc = new Scanner(new File("input.txt"));
+			
+			int pos=sc.nextInt(), x,y;
+			boolean[] arr = new boolean[3];
+			
+			arr[pos-1]=true;
+			for(int i=0;i<3;++i){
+				x=sc.nextInt();y=sc.nextInt();
+				boolean tmp=arr[x-1];
+				arr[x-1]=arr[y-1];
+				arr[y-1]=tmp;
+			}
+			
+			try {
+				PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
+				
+				for(int i=0;i<3;++i)
+					if(arr[i])
+						writer.println(i+1);
+				
+				writer.close();
+				
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+
+			
+			sc.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+}
