9c7430644f566a598276bb58bd7fde9618e6218a
abdou@gmail.com <abdou@gmail.com>
2016-08-08 11:11:48 +0300

    D

--- src/main/java/abdou/APlateGame.java
+++ src/main/java/abdou/APlateGame.java
@@ -1,7 +1,22 @@
 package abdou;
 
+import java.util.Scanner;
+
 /**
  * Created by fox on 27/07/2016.
  */
 public class APlateGame {
+    public static void main(String []args){
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int r = input.nextInt();
+
+        if(Math.min(a,b) >= 2*r){
+            System.out.println("First");
+        }else{
+            System.out.println("Second");
+        }
+
+    }
 }
